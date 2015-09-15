d23f0dc9abd87da42243e575bb28d6eba18dee02
Brian King <bri@dufflite.com>
2015-09-15 16:33:37 -0700

    Missed Bolus

---
+++ Nutshell/DataModel/CoreData/Bolus.swift
@@ -0,0 +1,17 @@
+//
+//  Bolus.swift
+//  Nutshell
+//
+//  Created by Brian King on 9/15/15.
+//  Copyright © 2015 Tidepool. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Bolus: CommonData {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
