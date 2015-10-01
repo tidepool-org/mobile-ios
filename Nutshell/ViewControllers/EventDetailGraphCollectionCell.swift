bed86e2be16d744b9e23492ecc052e35b9a9c1dc
Larry Kenyon <larry@dufflite.com>
2015-10-01 11:37:54 -0700

    Start on graph scrolling. Try using a simple collection view so we only draw parts the user wants to see.

---
+++ Nutshell/ViewControllers/EventDetailGraphCollectionCell.swift
@@ -0,0 +1,25 @@
+//
+//  EventDetailGraphCollectionCell.swift
+//  Nutshell
+//
+//  Created by Larry Kenyon on 10/1/15.
+//  Copyright © 2015 Tidepool. All rights reserved.
+//
+
+import UIKit
+
+class EventDetailGraphCollectionCell: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    func configureCell(eventItem: NutMeal) {
+        print("size at configure: \(self.frame.size)")
+        
+    }
+}
